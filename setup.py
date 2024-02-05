936459c53f24fe74bbc66a521db95db984f5b037
sylas <sylas@zz.com>
2024-02-05 10:52:14 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@ from setuptools import setup, find_packages
 setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    description="Python Client wrapper for Steam Web API",
+    keywords=[
+        "steam",
+        "steam api",
+        "steam community",
+        "steam web api"
+    ],
     name='py-steam-web-api',
+    url="https://github.com/sylas00/py-steam-web-api",
     version='0.1',
     author="sylas00",
     author_email="xyjy5247@gmail.com",
